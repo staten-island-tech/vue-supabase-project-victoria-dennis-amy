237837050835ded12c6d0ff3c4d8566038796e07
AmyChen159 <awesomeandcoolac@gmail.com>
2024-05-09 09:28:57 -0400

    hjh

--- supabase-vue-3/src/supabase.js
+++ supabase-vue-3/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://bswhnfykqdncsmdtesjv.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzd2huZnlrcWRuY3NtZHRlc2p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzMjE4NzUsImV4cCI6MjAyNzg5Nzg3NX0.YgOfIbpArBNcUBz6JfM-XphGj4tfigGPxOqvlb9wmxs"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
