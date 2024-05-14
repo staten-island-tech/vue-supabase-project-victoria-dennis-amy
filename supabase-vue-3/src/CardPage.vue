8c403b5e61932892a030669d663a07c50395bc34
dennishvf <dennisk38@nycstudents.net>
2024-05-14 09:24:01 -0400

    kytfdjyt

---
+++ supabase-vue-3/src/CardPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+
+const { data, error } = await supabase
+  .from('countries')
+  .select("*")
+console.log(.select)
+  
+await supabase
+  .from('countries')
+  .insert({ id: 1, name: 'Denmark' })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
