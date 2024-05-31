785eb9cd3752292a8aed75c305fc783ffe93dfba
AmyChen159 <awesomeandcoolac@gmail.com>
2024-05-31 09:08:49 -0400

    KAMKSNAJS

---
+++ supabase-vue-3/src/components/Cards.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <h2>{{ name }}</h2>
+        <div v-if="session">
+            <Button v-if="toggled" @click="favorited"></Button>
+            <Button v-else @click="favorited"></Button>
+        </div>
+    </div>
+</template>
