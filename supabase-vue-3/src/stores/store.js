efb6d3e540f3fb5d64f7deef7bbcb7bbd409982e
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-06-06 21:43:37 -0400

    wipe :smile:

--- supabase-vue-3/src/stores/store.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia";
-import { ref } from "vue"
-
-export const useSessionStore = defineStore("session", () => {
-    const id = ref("")
-    function setSession(newID) {
-        id.value = newID
-    }
-}) 
